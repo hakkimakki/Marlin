957fbebdf5c37335b00fb445a8958fb3e65e5632
hakkimakki <raffael.anklin@gmail.com>
2019-05-28 16:12:33 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -447,9 +447,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 19.96
+  #define DEFAULT_Ki 1.43
+  #define DEFAULT_Kd 69.52
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
